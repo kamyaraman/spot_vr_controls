6b229c2942f4ed5e58e8b67e5bcdb84fe1abbae5
sjuknelis <simonas_juknelis@brown.edu>
2025-07-01 14:46:34 -0400

    gaze detection on labels

--- cubeOperationTest/Assets/Labeller.cs
+++ cubeOperationTest/Assets/Labeller.cs
@@ -3,6 +3,8 @@ using TMPro;
 
 public class Labeller : MonoBehaviour
 {
+    public Transform controller;
+
     public Transform aXLabel,
         bYLabel,
         joystickLabel,
@@ -37,11 +39,18 @@ public class Labeller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bYLabel.rotation
-            = joystickLabel.rotation
-            = oculusMenuLabel.rotation
-            = indexLabel.rotation
-            = gripLabel.rotation
-            = aXLabel.rotation;
+        Transform[] labels = { aXLabel, bYLabel, joystickLabel, oculusMenuLabel, indexLabel, gripLabel };
+
+        for (int i = 1; i < labels.Length; i++)
+        {
+            labels[i].rotation = labels[0].rotation;
+        }
+
+        Vector3 toController = (controller.position - Camera.main.transform.position).normalized;
+        float gazeDelta = Vector3.Angle(Camera.main.transform.forward, toController);
+        foreach (Transform label in labels)
+        {
+            label.gameObject.SetActive(gazeDelta <= 15f);
+        }
     }
 }
